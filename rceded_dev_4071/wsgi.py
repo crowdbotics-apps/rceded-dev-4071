5698b7abe1bfe97fd2da2d245359f2172a42945c
Crowdbotics <team@crowdbotics.com>
2020-05-04 14:11:24 +0000

    Initial commit.

---
+++ rceded_dev_4071/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rceded_dev_4071 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rceded_dev_4071.settings')
+
+application = get_wsgi_application()
